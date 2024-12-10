6abfc64ab781d1891d6fb082ec84f96ebb31165d
HajduIstvan02 <hajduistvan@ugmszi.hu>
2024-12-10 12:57:37 +0100

    most már jó a kiindulás

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,10 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-import "bootstrap-icons/font/bootstrap-icons.min.css"
-
 import App from './App.vue'
 import router from './router'
 
