a0851d14dea283b1ef74fed627c1ca8a616cd163
HajduIstvan02 <hajduistvan@ugmszi.hu>
2024-12-10 12:37:41 +0100

    kiindulási pont

---
+++ Frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+export default{
+
+}
+</script>
+
+<template>
+  <div class="container-fluid my-border my-container">
+
+    <h1>CRUD műveletek</h1>
+    
+    <nav class="my-border p-4">
+      <RouterLink to="/">Home</RouterLink> | 
+
+    </nav>
+    
+    <div class="my-border p-2">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
