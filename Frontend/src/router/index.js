0f777bc663a4883862c55a21e45d7be63ed313f4
HajduIstvan02 <hajduistvan@ugmszi.hu>
2024-12-10 14:04:35 +0100

    befejezetlen

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -9,21 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    // {
-    //   path: '/1',
-    //   name: '1',
-    //   component: () => import('../views/1.vue'),
-    // },
-    // {
-    //   path: '/2',
-    //   name: '2',
-    //   component: () => import('../views/2.vue'),
-    // },
-    // {
-    //   path: '/3',
-    //   name: '3',
-    //   component: () => import('../views/3.vue'),
-    // },
     {
       path: '/Cards',
       name: 'Cards',
